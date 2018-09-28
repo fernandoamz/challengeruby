16299152b6cd4c865cfb0a967c7e4c7b2e56ab36
fernando amezcua alcantar <fernandoamezcuaalcantar@MacBook-Pro-de-fernando.local>
2018-09-28 11:49:55 -0500

    creando todo list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :todo_lists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root 'welcome#index'
+  root 'todo_lists#index'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_161546) do
+ActiveRecord::Schema.define(version: 2018_09_28_164404) do
+
+  create_table "todo_lists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
