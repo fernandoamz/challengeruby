0d71fcb97402ffe591eeb48ffebb4aed0e00dc49
fernando amezcua alcantar <fernandoamezcuaalcantar@MacBook-Pro-de-fernando.local>
2018-10-04 17:38:55 -0500

    Revert "add sidekiq config"
    This reverts commit 86122902b1cf6835c8e9a1b0866e969c2dd5adb8.

--- app/jobs/send_pending_task_job.rb
+++ app/jobs/send_pending_task_job.rb
@@ -3,14 +3,7 @@ class SendPendingTaskJob < ApplicationJob
 
   def perform(*args)
     # Do something later
-    @user = User.all
-
-    @user.each do |user|
-      mail = TodoMailer.task_pending(id: user.id, email: user.email)
-      puts "**************************"
-      puts mail
-      puts "**************************"
-      mail.deliver_now
-    end
+    mail = TodoMailer.task_pending
+    mail.deliver_now
   end
 end
