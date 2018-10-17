ea0345e32a31796af0a9edbdb9c91b7a6e808cf3
Fernando Amezcua <fernandoamz148@gmail.com>
2018-10-16 20:20:48 -0500

    Revert "add variable enviroments"
    This reverts commit 557c34aa1bf14bfe88e1a8e848c3e2e452583033.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'example.com',
-    user_name:            ENV["MAIL_USERNAME"],
-    password:             ENV["MAIL_PASSWORD"],
+    user_name:            'fernandoamz148@gmail.com',
+    password:             '',
     authentication:       'plain',
     enable_starttls_auto: true 
   }
--- db/migrate/20181010230210_change_column_user.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnUser < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :todo_lists, :user_id, :user_id
-  end
-end
