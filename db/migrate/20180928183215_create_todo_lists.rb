006f569155b8abdfb59392e5aa3f3f1c561b1e11
Fernando Amezcua <fernandoamz148@gmail.com>
2018-10-22 17:48:47 -0500

    change name migration

--- db/migrate/20180928183215_create_todo_lists.rb
+++ db/migrate/20180928183215_create_todo_lists.rb
@@ -3,7 +3,7 @@ class CreateTodoLists < ActiveRecord::Migration[5.2]
     create_table :todo_lists do |t|
       t.string :title
       t.string :description
-      t.integer :id_usuario
+      t.integer :id_user
       t.boolean :done_homework
       
       t.timestamps
