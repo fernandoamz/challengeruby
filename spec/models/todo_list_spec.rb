57bc9fb5239d11adc5d997e0e2bcb625c00ae990
Fernando Amezcua <fernandoamz148@gmail.com>
2018-10-29 12:59:09 -0600

    fix issues HCI

--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -1,45 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe TodoList, type: :model do
-  context 'validations test' do 
-    it 'ensures title precense' do 
+  let (:params) {{ title: 'test', description: 'test description', done_homework: 1, user_id: 2 }}
+
+  context 'validations test' do
+    it 'ensures title precense' do
       todo_list = TodoList.new(description: 'testing', done_homework: true).save
       expect(todo_list).to eq(false)
-    end  
+    end
 
-    it 'ensures description precense' do 
+    it 'ensures description precense' do
       todo_list = TodoList.new(title: 'testing', done_homework: true).save
       expect(todo_list).to eq(false)
-    end  
+    end
 
-    it 'ensures insert is correctly' do 
-      todo_list = TodoList.new(title: 'testing', description: 'testing', user_id: 1, done_homework: true).save
+    it 'ensures insert is correctly' do
+      todo_list = TodoList.new(params).save
       expect(todo_list).to eq(true)
-    end  
+    end
 
-    it 'ensures that is saving relation' do 
+    it 'ensures that is validating relation' do
       todo_list = TodoList.new(title: 'testing', description: 'testing', done_homework: true).save
       expect(todo_list).to eq(false)
-    end  
+    end
   end 
 
   context 'ensures TodoList is saving' do 
-    it 'ensures that show is working' do 
-      todo_list_show = TodoList.find(1) 
-      expect(todo_list_show.id).to eq(1)
-    end
-  end
-
-  context 'ensures TodoList is saving' do 
-    it 'ensures todo lists is saving' do 
-      todo_list_save = TodoList.new(title: 'test', description: 'test description', done_homework: 1, user_id: 2).save
+    it 'ensures todo lists is saving' do
+      todo_list_save = TodoList.new(params).save
       expect(todo_list_save).to eq(true)
     end
   end
 
-  context 'review update TodoList' do 
+  context 'review update TodoList' do
     it 'ensures that the TodoList is Updating content' do
-      todo_list_update = TodoList.find(3).update(title: 'test', description: 'test description', done_homework: 1, user_id: 2)
+      todo_list_update = TodoList.find(3).update(params)
       expect(todo_list_update).to eq(true)
     end
   end
